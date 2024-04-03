33f9f842d0f90d906ee705dde24d19595a87040c
bubleman1 <tgrobot5@gmail.com>
2024-04-03 04:03:37 +0300

    got the basic game working, got a few bugs

--- scripts.js
+++ scripts.js
@@ -1,12 +1,14 @@
 let money = 0;
 let totalMoney = 0;
 let rebirths = 0;
-let currencyName = "money";
+let universeNumber = 0;
 
 let buttonValue = 1;
 let buttonMultiplier = 1.00;
 let buttonPassiveIncome = 0;
 let buttonPassiveIncomeMultiplier = 1;
+
+let moneyNeededForRebirth = 100;
 
 let buttonUpgrades = [
 	{
@@ -46,23 +48,36 @@ let buttonUpgrades = [
 	}
 	];
 
-let currencyPossibleNames = ["money", "mony", "dolaridoos", "chedah", "dabloons", "gold", "amethysts", "diamonds", "copper", "silver", 
+let currencyPossibleNames = ["money", "mony", "dolaridoos", "chedah", "dabloons", "gold", "amethysts", "diamonds", "copper", "silver",  //38 values|10|9|9|10
 							 "cookies", "drams of water", "coal", "kilowats", "soap", "paperclips", "metres of real estate", "salt",
 							 "cinnamon", "sugar", "organs", "blood", "string", "distance traveled", "xp", "magic", "black matter",
 							 "planets", "germs", "tennis balls", "code", "humans", "titanium", "houses", "scrap", "freinds", "love",
 							 "bananas", "cucumbers", "glizzies", "subscribers", "thingamajigs", "dirt", "bottle caps", "golden rings"];
+let buttonPossibleText = ["Make me rich!", "Mek me rch!", "Make me rich!", "Make me some dough!", "Ma'ek me booty!", "Make me mine!", "Make me mine!", "Make me mine!", "Make me mine!", "Make me mine!", 
+							 "Make me cook!", "Make me qud!", "Make me mine!", "Make me energy!", "Make me clean!", "Make Paperclip", "Make me expand!", "Make me salty!",
+							 "Make me crush sticks!", "Make me die!", "Make me harvest!", "Make me donate!", "Make me spin!", "Make me run!", "Make me level up!", "Make me powerful!", "Make me interstellar rich!",
+							 "Make me an overlord!", "Make me infected!", "Make me a tennis pro!", "Make me a programmer!", "Make me a civilization!", "Make me mine!", "Make me build!", "Make me scavange!", "Make me friendships!", "Make my wife like me!",
+							 "Make banana!", "Make me pickle!", "Make me gobble!", "Make me famous!", "Make me invent!", "Make me scrape!", "Make me fallout rich!", "Make me go fast!"];
 
 const mainButton = document.querySelector("#mainButton");
 const currencyText = document.querySelector("#currencyText");
 const currencyNameText = document.querySelector("#currencyNameText");
+const moneyNeededForRebirthValueText = document.querySelector("#moneyNeededForRebirthValueText");
+const moneyNeededForRebirthText = document.querySelector("#moneyNeededForRebirthText");
+const rebirthValText = document.querySelector("#rebirthValText");
 
 //Initialize buttons
 mainButton.onclick = getClickMoney;
 
 function getClickMoney(){
-	money += floor(buttonValue * buttonMultiplier);
+	money += Math.floor(buttonValue * buttonMultiplier);
 	currencyText.innerText = money;
 	console.log(money + " " + buttonMultiplier);
+	if(money >= moneyNeededForRebirth)
+	{
+		mainButton.onclick = doRebirth;
+		mainButton.innerText = "Rebirth";
+	}
 }
 function upgradeButton(number){
 	let currentUpgrade = buttonUpgrades[number];
@@ -101,16 +116,32 @@ function upgradeButton(number){
 	}
 }
 
-function Rebirth()
+function doRebirth()
 {
+	//Reset values and calculate base income
 	money = 0;
 	totalMoney = 0;
 	rebirths += 1;
-	currencyName = "money";
+	rebirthValText.innerText = rebirths;
 	buttonValue = 1 + rebirths;
-	buttonMultiplier = 1.00 + floor(rebirths / 10);
-	buttonPassiveIncome = 0 + floor(rebirths/2);
-	buttonPassiveIncomeMultiplier = floor(rebirths / 10);
+	moneyNeededForRebirth += Math.floor(moneyNeededForRebirth * 0.66);
+	buttonMultiplier = 1.00 + rebirths * 0.2;
+	buttonPassiveIncome = 0 + Math.floor(rebirths/2);
+	buttonPassiveIncomeMultiplier = Math.floor(rebirths / 10);
+
+	//Universe switch
+	let lastUniverse = universeNumber;
+	while(universeNumber==lastUniverse)
+	{
+		universeNumber = Math.floor(Math.random() * 38);
+	}	
+	console.log("Universe number: " + universeNumber);
+	currencyNameText.innerText = currencyPossibleNames[universeNumber] + ": ";
+	moneyNeededForRebirthText.innerText = currencyPossibleNames[universeNumber] + " needed for next rebirth: ";
+	moneyNeededForRebirthValueText.innerText = moneyNeededForRebirth; //BUG: moneyNeededForRebirthValue dissapears after 1st rebirth.
+	mainButton.innerText = buttonPossibleText[universeNumber];
+
+	mainButton.onclick = getClickMoney;
 
 	//Rebirth upgrades are applied here.
 }
